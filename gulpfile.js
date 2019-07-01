348305c0941b48c1003ab026058b2ee58741285e
Volodymyr Dedenko <dedenkof@ukr.net>
2019-07-02 01:08:38 +0300

    Release v1

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 'use strict';
-
+// Del autoprefixer, gulp-group-css-media-queries
 const gulp = require('gulp'),
-    autoprefixer = require('gulp-autoprefixer'),
+    autoprefixer = require('autoprefixer'),
+    postcss = require('gulp-postcss'),
     concat = require('gulp-concat'),
     svgmin = require('gulp-svgmin'),
     sass = require('gulp-sass'),
@@ -22,14 +23,12 @@ const gulp = require('gulp'),
     googleWebFonts = require('gulp-google-webfonts'),
     replace = require('gulp-replace'),
     htmlmin = require('gulp-htmlmin'),
-    cache = require('gulp-cache'),
     flatten = require('gulp-flatten'),
     gifsicle = require('imagemin-gifsicle'),
     jpegtran = require('imagemin-jpegtran'),
     optipng = require('imagemin-optipng'),
     stylelint = require('stylelint'),
     /* Generate favicon */
-    realFavicon = require('gulp-real-favicon'),
     fs = require('fs'),
     /* //Generate favicon */
     npmDist = require('gulp-npm-dist');
@@ -43,6 +42,8 @@ const path = {
         css: 'build/css/',
         img: 'build/img/',
         fonts: 'build/fonts/',
+        sprites: 'build/img/sprites/',
+        spritesCss: 'build/css/partial/',
         svg: 'build/img/svg/',
         injectCSS: 'build/css/**/*.css',
         injectFontsCSS: 'build/fonts/**/*.css',
@@ -53,27 +54,25 @@ const path = {
     inject: {
         css: {
             bootstrap: 'build/css/bootstrap/bootstrap.min.css',
-            bootstrapGrid: 'build/css/bootstrap/bootstrap-grid.min.css',
-            animate: 'build/css/animate/animate.min.css',
             custom: 'build/css/custom.min.css'
         },
         js: {
-            jsHTML5: {
-                es5: 'build/js/html5shiv/es5-shim.min.js',
-                print: 'build/js/html5shiv/html5shiv-printshiv.min.js',
-                html5shiv: 'build/js/html5shiv/html5shiv.min.js'
+            jsHTML5:{
+                es5: 'build/js/html5shiv/es5-shim.min.min.js',
+                print: 'build/js/html5shiv/html5shiv-printshiv.min.min.js',
+                html5shiv: 'build/js/html5shiv/html5shiv.min.min.js'
             },
             jquery: 'build/js/jquery/jquery.min.js',
-            rangeslider: 'build/js/rangeslider/rangeslider.min.js',
-            jqPayment: 'build/js/jqpayment/jquery.payment.min.js',
             main: 'build/js/main.min.js'
 
         }
     },
     src: {
         src: 'src/',
+        php: 'src/**/**/**/*.php',
         mainHTML: 'src/index.html',
         html: 'src/*.html',
+        robotsTXT: 'src/robots.txt',
         js: 'src/js/*.js',
         jsLib: 'src/js/libs/**/*.js',
         css: 'src/css/**/*.css',
@@ -82,13 +81,14 @@ const path = {
         img: 'src/img/**/*.{png,xml,jpg,gif,svg,ico,webmanifest}',
         fonts: 'src/fonts/**/*.*',
         fontsGoogle: 'src/fonts/',
+        sprites: 'src/img/sprites/*.png',
+        svgSprites: 'src/img/sprites/svg-sprite/',
         svg: 'src/img/uploads/svg-sprite-pack/**/*.svg',
         extSVG: '.svg',
         extPNG: '.png'
     },
     libsCSS: {
-        bootstrapCSS: 'node_modules/bootstrap/dist/css/bootstrap.css',
-        bootstrapGrid: 'node_modules/bootstrap/dist/css/bootstrap-grid.css',
+        bootstrapCSS: 'node_modules/bootstrap/dist/css/bootstrap.css'
     },
     libsJS: {
         jquery: 'node_modules/jquery/dist/jquery.js',
@@ -96,11 +96,15 @@ const path = {
     },
     watch: {
         html: 'src/**/*.html',
+        php: 'src/**/*.php',
+        pug: 'src/pug/**/*.pug',
+        robotsTXT: 'src/robots.txt',
         js: 'src/js/**/*.js',
         css: 'src/css/**/*.scss',
         sass: 'src/sass/**/*.scss',
         img: 'src/img/**/*.*',
         fonts: 'src/fonts/**/*.*',
+        sprites: 'src/img/sprites/*.png',
         svg: 'src/img/svg/**/*.svg'
     },
 
@@ -117,17 +121,7 @@ const options = {
     cssFilename: 'googlefonts.css'
 };
 
-// Config autoprefixer add prefix to the browsers
-const autoprefixerList = [
-    'Chrome >= 45',
-    'Firefox ESR',
-    'Edge >= 12',
-    'Explorer >= 10',
-    'iOS >= 9',
-    'Safari >= 9',
-    'Android >= 4.4',
-    'Opera >= 30'
-];
+
 
 // browserSync config
 const config = {
@@ -143,13 +137,36 @@ const config = {
 };
 
 // Notify error
-const onError = function (err) {
+const onError = function(err) {
     notify.onError({
         title: 'Error in ' + err.plugin,
     })(err);
     this.emit('end');
 };
 
+
+// Listing package.json dependencies and copy css libs files of them  to build/css
+gulp.task('copyLibsCSS', () =>
+    gulp.src(npmDist({
+        excludes: [
+            '/**/*.txt',
+            '/**/*.js',
+            '/**/bootstrap-reboot.min.css'
+        ]
+    }), { base: './node_modules' })
+        .pipe(flatten({ includeParents: 1}))
+        .pipe(sourcemaps.init())
+        //.pipe(postcss([ autoprefixer() ]))
+        //.pipe(gcmq())
+        //.pipe(concat('libs.css'))
+        //.pipe(rename({suffix: '.min'})) // Добавляем в название файла суфикс .min
+        .pipe(cleanCSS({level: 2}))
+        .pipe(sourcemaps.write('.', {addComment: false}))
+        .pipe(gulp.dest(path.build.css))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+);
 
 // Listing package.json dependencies and copy js libs files of them  to build/css
 gulp.task('copyLibsJS', () =>
@@ -161,11 +178,13 @@ gulp.task('copyLibsJS', () =>
             '/**/jquery.slim.min.js',
             '/**/bootstrap.bundle.min.js'
         ]
-    }), {base: './node_modules'})
-        .pipe(flatten({includeParents: 1}))
-        .pipe(plumber({errorHandler: onError}))
+    }), { base: './node_modules' })
+        .pipe(flatten({ includeParents: 1}))
+        .pipe(plumber({ errorHandler: onError }))
         .pipe(sourcemaps.init())
-        .pipe(minJS())
+        //.pipe(minJS())
+        //.pipe(concat('libs.js'))
+        //.pipe(rename({suffix: '.min'}))
         .pipe(sourcemaps.write('.', {addComment: false}))
         .pipe(gulp.dest(path.build.js))
         .pipe(browserSync.reload({
@@ -176,7 +195,13 @@ gulp.task('copyLibsJS', () =>
 // Deploy html files (rigger template from ./tempalte )
 gulp.task('html', () =>
     gulp.src(path.src.html)
-        .pipe(plumber({errorHandler: onError}))
+        .pipe(plumber({ errorHandler: onError }))
+        .pipe(includeFiles())
+        //.pipe(realFavicon.injectFaviconMarkups(JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).favicon.html_code))
+        /*.pipe(htmlmin({
+         collapseWhitespace: true,
+         removeComments: false
+         }))*/
         .pipe(replace(/\n\s*<!--DEV[\s\S]+?-->/gm, ''))
         .pipe(gulp.dest(path.build.html))
         .on('end', browserSync.reload)
@@ -186,11 +211,11 @@ gulp.task('html', () =>
 // Deploy css via sass preprocessor
 gulp.task('sass', () =>
     gulp.src(path.src.sass)
-        .pipe(plumber({errorHandler: onError}))
+        .pipe(plumber({ errorHandler: onError }))
         .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: 'expanded'}))
-        .pipe(autoprefixer({browsers: autoprefixerList, cascade: false}))
-        .pipe(gcmq())
+        // .pipe(postcss([ autoprefixer() ]))
+        //.pipe(gcmq())
         .pipe(concat('custom.css'))
         .pipe(rename({suffix: '.min'}))
         .pipe(cleanCSS({level: 2}))
@@ -199,32 +224,52 @@ gulp.task('sass', () =>
         .pipe(browserSync.reload({
             stream: true
         }))
+
 );
 
+// Copy src/css/libs and node_modules source to build/css
+gulp.task('cssLibs', () =>
+    gulp.src(path.src.cssLib)
+        .pipe(plumber({ errorHandler: onError }))
+        .pipe(sourcemaps.init())
+        //.pipe(postcss([ autoprefixer() ]))
+        //.pipe(gcmq())
+        //.pipe(concat('libs.css'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(cleanCSS({
+            level: 2
+        }))
+        .pipe(sourcemaps.write('.', {addComment: false}))
+        .pipe(gulp.dest(path.build.css))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+);
 
 // Copy src/js/libs and node_modules source to build/js
 gulp.task('scriptsLibs', () =>
     gulp.src(path.src.jsLib)
         .pipe(includeFiles())
-        .pipe(plumber({errorHandler: onError}))
+        .pipe(plumber({ errorHandler: onError }))
         .pipe(sourcemaps.init())
-        .pipe(minJS())
+        //.pipe(minJS())
         ///.pipe(concat('libs.js'))
-        //.pipe(rename({suffix: '.min'}))
+        .pipe(rename({suffix: '.min'}))
         .pipe(sourcemaps.write('.', {addComment: false}))
         .pipe(gulp.dest(path.build.js))
         .pipe(browserSync.reload({
             stream: true
         }))
+
 );
 
 // Transfer custom js to build
 gulp.task('scripts', () =>
     gulp.src(path.src.js)
         .pipe(includeFiles())
-        .pipe(plumber({errorHandler: onError}))
+        .pipe(plumber({ errorHandler: onError }))
         .pipe(sourcemaps.init())
-        .pipe(minJS())
+        //.pipe(minJS())
         //.pipe(concat('general.js'))
         .pipe(rename({suffix: '.min'}))
         .pipe(sourcemaps.write('.', {addComment: false}))
@@ -232,6 +277,7 @@ gulp.task('scripts', () =>
         .pipe(browserSync.reload({
             stream: true
         }))
+
 );
 
 // Include style, js, favicon and markup to main page
@@ -239,6 +285,7 @@ gulp.task('inject', (done) => {
 
     const injectStyles = gulp.src([
         path.build.injectFontsCSS,
+        path.inject.css.bootstrap,
         path.inject.css.custom
 
     ], {read: false});
@@ -253,7 +300,7 @@ gulp.task('inject', (done) => {
 
     gulp.src(path.src.html)
         .pipe(includeFiles())
-        .pipe(inject(injectStyles, {ignorePath: 'build', addRootSlash: false, relative: false}))
+        .pipe(inject(injectStyles, {ignorePath:  'build', addRootSlash: false, relative: false}))
         .pipe(inject(injectScripts, {ignorePath: 'build', addRootSlash: false, relative: false}))
 
         .pipe(gulp.dest(path.build.html))
@@ -264,13 +311,17 @@ gulp.task('inject', (done) => {
 });
 
 
+
+
+
+
 // Copy all generated img files into build directory
 gulp.task('images', () =>
     gulp.src([
         path.src.img,
     ])
         .pipe(gulp.dest(path.build.img))
-        .pipe(cache(imagemin([
+        .pipe(imagemin([
                 imagemin.gifsicle({interlaced: true}),
                 imagemin.jpegtran({progressive: true}),
                 imagemin.optipng({optimizationLevel: 8}),
@@ -280,10 +331,10 @@ gulp.task('images', () =>
                         {cleanupIDs: false}
                     ]
                 })
-            ], {
+            ],{
                 verbose: true // output status treatment img files
             }
-        )))
+        ))
 
         .pipe(gulp.dest(path.build.img))
         .pipe(browserSync.reload({
@@ -305,17 +356,7 @@ gulp.task('fonts', () =>
 );
 
 
-//testing your build html files
-gulp.task('validation', () =>
-    gulp.src(`${path.build.html}**/*.html`)
 
-        .pipe(html5Lint())
-);
-//testing your build css files
-gulp.task('cssLint', () =>
-    gulp.src(path.src.sass)
-        .pipe(stylelint())
-);
 
 // reload after change via browserSync
 gulp.task('serve', () => {
@@ -328,24 +369,20 @@ gulp.task('clean', (cb) =>
     rimraf(path.cleanBuild, cb)
 );
 
-// clear cashe images
-gulp.task('clearCache', () =>
-    cache.clearAll()
-);
 
 
-gulp.task('watch', function () {
+gulp.task('watch', function() {
     /*gulp.watch(path.watch.pug, gulp.series('pug'));
-    gulp.watch(path.watch.htaccess, gulp.series('htaccess'));*/
-    gulp.watch(path.watch.html, gulp.series('html', 'inject'));
+     gulp.watch(path.watch.htaccess, gulp.series('htaccess'));*/
+    gulp.watch(path.watch.html, gulp.series('html'));
     //gulp.watch(path.watch.php, gulp.series('php'));
     gulp.watch(path.watch.sass, gulp.series('sass'));
+    gulp.watch(path.watch.css, gulp.series('cssLibs'));
     gulp.watch(path.watch.js, gulp.series('scripts'));
     gulp.watch(path.watch.js, gulp.series('scriptsLibs'));
     gulp.watch(path.watch.img, gulp.series('images'));
     gulp.watch(path.watch.fonts, gulp.series('fonts'));
     // gulp.watch(path.watch.robotsTXT, gulp.series('robotsTXT'));
-    //gulp.watch(path.watch.favicons, gulp.series('generate-favicon'));
 });
 
 /*-- MANUALLY RUN TASK --*/
@@ -355,11 +392,9 @@ gulp.task('watch', function () {
 
 gulp.task('assetsBASE', gulp.series(['html']));  // IF need add 'pug', 'php', tasks
 
-gulp.task('assetsCSS', gulp.series(['sass'])); // IF need add stylus
+gulp.task('assetsCSS', gulp.series(['sass', 'cssLibs', 'copyLibsCSS'])); // IF need add stylus
 
-gulp.task('assetsJS', gulp.series(['scriptsLibs', 'copyLibsJS', 'scripts']));
-
-// gulp.task('assetsIMG', gulp.series(['generate-favicon', 'images']));
+gulp.task('assetsJS', gulp.series([ 'scriptsLibs', 'copyLibsJS', 'scripts']));
 
 gulp.task('assetsIMG', gulp.series(['images']));
 
